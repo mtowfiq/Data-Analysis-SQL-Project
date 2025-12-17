3217befd7f9d863417326877b8174d7d8cc4b9ab
Towfiq <mohammedthawfiq00@gmail.com>
2025-12-17 05:18:35 +0300

    Modified readme

--- sql_load/3_modify_tables.sql
+++ sql_load/3_modify_tables.sql
@@ -1,34 +1,3 @@
-/* ⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️⚠️
-Database Load Issues (follow if receiving permission denied when running SQL code below)
-
-Possible Errors: 
-- ERROR >>  duplicate key value violates unique constraint "company_dim_pkey"
-- ERROR >> could not open file "C:\Users\...\company_dim.csv" for reading: Permission denied
-
-1. Drop the Database 
-            DROP DATABASE IF EXISTS sql_course;
-2. Repeat steps to create database and load table schemas
-            - 1_create_database.sql
-            - 2_create_tables.sql
-3. Open pgAdmin
-4. In Object Explorer (left-hand pane), navigate to `sql_course` database
-5. Right-click `sql_course` and select `PSQL Tool`
-            - This opens a terminal window to write the following code
-6. Get the absolute file path of your csv files
-            1. Find path by right-clicking a CSV file in VS Code and selecting “Copy Path”
-7. Paste the following into `PSQL Tool`, (with the CORRECT file path)
-
-\copy company_dim FROM '[Insert File Path]/company_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-\copy skills_dim FROM '[Insert File Path]/skills_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-\copy job_postings_fact FROM '[Insert File Path]/job_postings_fact.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-\copy skills_job_dim FROM '[Insert File Path]/skills_job_dim.csv' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
-
-*/
-
--- NOTE: This has been updated from the video to fix issues with encoding
 COPY company_dim
 FROM 'D:\Data Science\SQL\Data Analysis SQL Project\csv_files\company_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
@@ -45,33 +14,6 @@ COPY skills_job_dim
 FROM 'D:\Data Science\SQL\Data Analysis SQL Project\csv_files\skills_job_dim.csv'
 WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 
-CREATE table january_jobs as 
-SELECT *
-FROM job_postings_fact
-WHERE EXTRACT(MONTH from job_posted_date) = 1
-
-CREATE table february_jobs as 
-SELECT *
-FROM job_postings_fact
-WHERE EXTRACT(MONTH from job_posted_date) = 2
-
-CREATE table march_jobs as 
-SELECT *
-FROM job_postings_fact
-WHERE EXTRACT(MONTH from job_posted_date) = 3
-
-
-select * 
-from job_postings_fact
-where job_location like '%Dubai%' AND job_title_short = 'Data Analyst'  AND salary_year_avg is not null
-
-
-select * from company_dim LIMIT 10;
-
-
-select job_id, job_title_short, EXTRACT(MONTH from job_posted_date) <= 3 as first_quarter
-from job_postings_fact
-where EXTRACT(MONTH from job_posted_date) <= 3
 
 
 
@@ -79,17 +21,9 @@ where EXTRACT(MONTH from job_posted_date) <= 3
 
 
 
--- I want the names of the companies with the most jobs
 
-with company_job_count AS (
-    select company_id, count(*) as total_jobs
-    from job_postings_fact
-    GROUP BY company_id
-)
 
-select company_dim.name as company_name, company_job_count.total_jobs
-from company_dim
-left join company_job_count on company_job_count.company_id = company_dim.company_id
-ORDER BY total_jobs desc
+
+
 
 
